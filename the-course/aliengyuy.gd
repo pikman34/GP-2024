20b697081c510f0d91258c1a0a13912652317e1e
Noah <noahbob34@gmail.com>
2024-10-07 17:03:02 +0100

    guy actually turns properly now lol

--- the-course/aliengyuy.gd
+++ the-course/aliengyuy.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@onready var position2D = $Marker2D
+
 const bulletPath = preload("res://laserbeam.tscn")
 
 const SPEED = 300.0
@@ -15,18 +15,30 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("turn_left", "turn_right")
-	if direction>0:
-		position2D.scale.x=1
-	elif direction<0:
-		position2D.scale.x=-1
 		
-	if direction:
+	if direction<0:
 		velocity.x = direction * SPEED
+		$flipnode/alienguy.flip_h = true	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		$flipnode/alienguy.flip_h = false
+		
+	if Input.is_action_just_pressed("fire"):
+		shoot()
+	
 	move_and_slide()
+
+func shoot():
+	var bullet = bulletPath.instance()
+	
+	get_parent().add_child(bullet)
+	bullet.position = $flipnode/wherelasercomefrom/Marker2D.global_position
+	
+	bullet.velocity = $Marker2D - bullet.position
+
+	
