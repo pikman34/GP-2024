116380054032250873a6e5f748fbe07489e355a1
Noah <noahbob34@gmail.com>
2024-10-07 16:46:14 +0100

    alien turning left and right

--- the-course/aliengyuy.gd
+++ the-course/aliengyuy.gd
@@ -1,22 +1,29 @@
 extends CharacterBody2D
 
+@onready var position2D = $Marker2D
+const bulletPath = preload("res://laserbeam.tscn")
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("turn_left", "turn_right")
+	if direction>0:
+		position2D.scale.x=1
+	elif direction<0:
+		position2D.scale.x=-1
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
