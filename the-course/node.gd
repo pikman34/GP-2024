4c26f6ae89c18f178127fce8f5f647c19a475cec
Noah <noahbob34@gmail.com>
2024-11-04 15:48:27 +0000

    scrawtacular

--- the-course/node.gd
+++ the-course/node.gd
@@ -1,12 +1,21 @@
-extends Node
+extends Node2D
 
 @export var thing:PackedScene
 @export var count:int = 12
 @export var radius = 500
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
 
+	var theta_inc:float = (PI * 2.0) / count
+	for i in range(count):
+		var theta = theta_inc * i
+		var x = sin(theta) * radius
+		var y = cos(theta) * radius
+		
+		var thing:Node2D = thing_scene.instantiate()
+		thing.position = Vector2D(x , y)
+		add_child(thing)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
