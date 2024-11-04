76e4179ede2dc4a3c0c18b7696eb05245dc5d972
Noah <noahbob34@gmail.com>
2024-11-04 15:40:35 +0000

    stuff

---
+++ the-course/node.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@export var thing:PackedScene
+@export var count:int = 12
+@export var radius = 500
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
