9e0d7bdb2b6ed69bbf927272555c0cf8bf00d7c7
Noah <noahbob34@gmail.com>
2024-11-18 16:03:48 +0000

    he go opposite direction

---
+++ the-course/skibidiright.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+@export var speed = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var r = get_viewport_rect()
+	global_position.x += speed * delta
+	if global_position.x > r.size.x:
+		global_position.x += -2 * speed * delta
+	pass
