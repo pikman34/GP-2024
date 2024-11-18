cbb09e05c6a3f04103e8d24c2d8bb67f19d1f460
Noah <noahbob34@gmail.com>
2024-11-18 14:54:18 +0000

    blue and red

--- the-course/if_statement.gd
+++ the-course/if_statement.gd
@@ -1,10 +1,20 @@
 extends Node2D
 
-func _draw() -> void:
-	var r = get_viewport_rect()
-	r.size.x = r.size.x / 2
-	draw_rect(Rect2(r), Color.RED, true, 10, true)
+var i = 0
 
+func _draw() -> void:
+	print("draw called: " + str(i))
+	var r = get_viewport_rect()
+	var h = r.size.x / 2
+	draw_rect(Rect2(r), Color.RED, true, 10, true)
+	var p = get_viewport().get_mouse_position()
+	print(p)
+	if p.x < h:
+		draw_rect(Rect2(0, 0, h, r.size.y), Color.RED, true, 10, true)
+		draw_rect(Rect2(h, 0, h, r.size.y), Color.BLUE, true, 10, true)
+	else: 
+		draw_rect(Rect2(0, 0, h, r.size.y), Color.BLUE, true, 10, true)
+		draw_rect(Rect2(h, 0, h, r.size.y), Color.RED, true, 10, true)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,4 +22,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	queue_redraw()
 	pass
