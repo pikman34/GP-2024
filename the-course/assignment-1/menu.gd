00fd56850ec4df10c194a80d368aa1e77bd36c3d
Noah <noahbob34@gmail.com>
2024-12-06 19:03:03 +0000

    added some stuff

--- the-course/assignment-1/menu.gd
+++ the-course/assignment-1/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://mainscreen.tscn")
 
 
 func _on_exit_pressed() -> void:
