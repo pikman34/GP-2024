c1202f21c071e50196b199010ca74db96b14e017
Noah <noahbob34@gmail.com>
2025-02-20 12:45:41 +0000

    tween editing

--- the-course/ufo.gd
+++ the-course/ufo.gd
@@ -23,7 +23,7 @@ func _ready() -> void:
 	# Tween my scale using elastic	
 	if ! Engine.is_editor_hint():
 		scale = Vector2.ZERO
-		var tween = create_tween().set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_IN_OUT)
+		var tween = create_tween().set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN_OUT)
 		tween.tween_property(self, "scale", Vector2.ONE, 1)
 		# vary the pitch
 		$Synth.pitch_scale = randf_range(0.7, 1.3)
