08b70a784f7244bedd57cc1388123c306a47af0e
Noah <noahbob34@gmail.com>
2025-02-27 15:35:16 +0000

    working guy chasing after me aahhh scary

--- the-course/dude_enemy.gd
+++ the-course/dude_enemy.gd
@@ -2,8 +2,13 @@ extends CharacterBody2D
 
 @export var speed = 200
 
+var color:Color = Color.WHITE
+
 var dude
 
+enum State {IDLE, ATTACKING}
+
+var state:State = State.IDLE
 
 
 func _ready() -> void:
@@ -16,3 +21,14 @@ func _physics_process(delta: float) -> void:
 	var theta = atan2(to_player.y, to_player.x) + (PI * 1)
 	rotation = theta
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == dude:
+		state = State.ATTACKING
+	pass
+	
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body == dude:
+		state = State.ATTACKING
+	pass # Replace with function body.
